617b6b3d9810eb12255e50bc6575b5b9d35a54ff
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-09 17:16:09 +1000

    fixed python class attribute error

--- code/models/VAE.py
+++ code/models/VAE.py
@@ -6,15 +6,15 @@ class Encoder(nn.Module):
 
     def __init__(self, layers_dim, activation_func):
         super(Encoder, self).__init__()
-        self.layers = self._build(layers_dim)
-        self.activation_func = activation_func
+        self.layers = self._build(layers_dim, activation_func)
 
-    def _build(self, layers_dim):
+    def _build(self, layers_dim, activation_func):
         layers = []
         for i in range(1, len(layers_dim)):
             layer = nn.Linear(layers_dim[i-1], layers_dim[i])
             layers.append(layer)
-            layers.append(self.activation_func)
+            layers.append(activation_func)
+        print(layers)
         return nn.Sequential(*layers)
     
     def forward(self, x):
@@ -24,17 +24,16 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
     
-    def __init__(self, layers_dim, activation_func):
+    def __init__(self, layers_dim, internal_activation, output_activation):
         super(Decoder, self).__init__()
-        self.layers = self._build(layers_dim)
-        self.activation_func = activation_func
+        self.layers = self._build(layers_dim, internal_activation, output_activation)
     
-    def _build(self, layers_dim):
+    def _build(self, layers_dim, internal_activation, output_activation):
         layers = []
         for i in range(1, len(layers_dim)):
             layer = nn.Linear(layers_dim[i-1], layers_dim[i])
             layers.append(layer)
-            layers.append(self.activation_func)
+            layers.append(internal_activation if i < len(layers_dim) - 1 else output_activation)
         return nn.Sequential(*layers)
     
     def forward(self, x):
@@ -44,13 +43,11 @@ class Decoder(nn.Module):
 
 class VAE(nn.Module):
 
-    def __init__(self, encoder_dim, encoder_activation, z_dim, decoder_dim, decoder_activation, output_dim,  output_activation):
+    def __init__(self, encoder_dim, encoder_activation, z_dim, decoder_dim, decoder_activation, output_activation):
         super(VAE, self).__init__()
         self.encoder = Encoder(layers_dim=encoder_dim, activation_func=encoder_activation)
         self.latent_layer = nn.Linear(encoder_dim[-1], z_dim)
-        self.decoder = Decoder(layers_dim=decoder_dim, activation_func=decoder_activation)
-        self.output_layer = nn.Linear(decoder_dim[-1], output_dim)
-        self.output_activation = output_activation
+        self.decoder = Decoder(layers_dim=decoder_dim, internal_activation=decoder_activation, output_activation=output_activation)
     
     def sampling(self, mean, log_var):
         sigma = torch.exp(log_var / 2)
@@ -64,6 +61,8 @@ class VAE(nn.Module):
         mean, log_var = self.latent_layer(x), self.latent_layer(x)
         z = sampling(mean, log_var)
         x = self.decoder(z)
-        x = self.output_layer(x)
-        x = self.output_activation(x)
         return x
+
+if __name__ == "__main__":
+    vae = VAE([784, 400], nn.LeakyReLU(), 20, [20, 400, 784], nn.LeakyReLU(), nn.Sigmoid())
+    print(vae)
