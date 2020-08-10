2d96af7f2ffd52ac92e38dd5c0eb7c1cf348760d
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-10 16:32:44 +1000

    added the code for the decoder class

--- code/models/ConvolutionalVAE.py
+++ code/models/ConvolutionalVAE.py
@@ -17,3 +17,20 @@ class Encoder(nn.Module):
     def forward(self, x):
         x = self.layers(x)
         return x
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.layers = self._build(channels, kernels, strides, paddings, internal_activation, output_activation)
+    
+    def _build(self, channels, kernels, strides, paddings, internal_activation, output_activation):
+        layers = []
+        for i in range(1, len(channels)):
+            layer = nn.Conv2d(channels[i-1], channels[i], kernels[i], strides[i], paddings[i])
+            layers.append(layer)
+            layers.append(internal_activation if i < len(channels) - 1 else output_activation)
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.layers(x)
+        return x
