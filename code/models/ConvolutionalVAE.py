7bed581b10324ef817b62fd36f28920cd2abf866
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-10 19:49:56 +1000

    changed the implementation of convolutional vae

--- code/models/ConvolutionalVAE.py
+++ code/models/ConvolutionalVAE.py
@@ -5,7 +5,7 @@ import numpy as np
 class Encoder(nn.Module):
     def __init__(self, channels, kernels, strides, paddings, activation_func):
         super(Encoder, self).__init__()
-        self.layers = self._build(channels, kernels, strides, paddings, activation_func)
+        self.conv = self._build(channels, kernels, strides, paddings, activation_func)
     
     def _build(self, channels, kernels, strides, paddings, activation_func):
         layers = []
@@ -16,15 +16,12 @@ class Encoder(nn.Module):
         return nn.Sequential(*layers)
     
     def forward(self, x):
-        x = self.layers(x)
+        x = self.conv(x)
         return x
 
 class Decoder(nn.Module):
-    def __init__(self, z_dim, unflatten_dim, channels, kernels, strides, paddings, internal_activation, output_activation):
+    def __init__(self, channels, kernels, strides, paddings, internal_activation, output_activation):
         super(Decoder, self).__init__()
-        # connecting the latent space to the fc layer
-        self.fc = nn.Linear(z_dim, np.prod(unflatten_dim))
-        self.unflatten_dim = unflatten_dim
         self.conv = self._build(channels, kernels, strides, paddings, internal_activation, output_activation)
     
     def _build(self, channels, kernels, strides, paddings, internal_activation, output_activation):
@@ -37,49 +34,64 @@ class Decoder(nn.Module):
         return nn.Sequential(*layers)
     
     def forward(self, x):
-        x = self.fc(x)
-        # unflatten the output and feed it to convolution layers
-        x = x.view(-1, *self.unflatten_dim)
         x = self.conv(x)
         return x
 
 class ConvolutionalVAE(nn.Module):
     def __init__(self, 
+        input_shape,
         e_channels, e_kernels, e_strides, e_paddings, e_activation_func, 
         z_dim,
-        unflatten_dim, d_channels, d_kernels, d_strides, d_paddings, d_internal_activation, d_output_activation
+        d_channels, d_kernels, d_strides, d_paddings, d_internal_activation, d_output_activation
     ):
         super(ConvolutionalVAE, self).__init__()
+        # create a convolution encoder
         self.encoder = Encoder(e_channels, e_kernels, e_strides, e_paddings, e_activation_func)
         self.z_dim = z_dim
-        self.decoder = Decoder(z_dim, unflatten_dim, d_channels, d_kernels, d_strides, d_paddings, d_internal_activation, d_output_activation)
+        # create a convolution decoder
+        self.decoder = Decoder(d_channels, d_kernels, d_strides, d_paddings, d_internal_activation, d_output_activation)
+        # get the shape to be used for creating the fc layer in both encode and decode function
+        self.unflatten_shape = self._initialize(input_shape)
+    
+    def _initialize(self, input_shape):
+        x = self.encoder(torch.unsqueeze(torch.randn(input_shape), 0))
+        return x.shape[1:]
     
     def sampling(self, mean, log_var):
         sigma = torch.exp(log_var / 2)
         epsilon = torch.randn_like(sigma)
         return mean + sigma * epsilon
     
-    def forward(self, x):
+    def encode(self, x):
         x = self.encoder(x)
-        flatten_shape = x.shape[1] * x.shape[2] * x.shape[3]
-        # flatten
+        flatten_shape = np.prod(self.unflatten_shape)
         x = x.view(-1, flatten_shape)
         mean, log_var = nn.Linear(flatten_shape, self.z_dim)(x), nn.Linear(flatten_shape, self.z_dim)(x)
         z = self.sampling(mean, log_var)
-        x = self.decoder(z)
-
+        return mean, log_var, z
+    
+    def decode(self, z):
+        flatten_shape = np.prod(self.unflatten_shape)
+        x = nn.Linear(self.z_dim, flatten_shape)(z)
+        x = x.view(-1, *self.unflatten_shape)
+        x = self.decoder(x)
+        return x
+    
+    def forward(self, x):
+        mean, log_var, z = self.encode(x)
+        x = self.decode(z)
         return mean, log_var, x
 
 
 if __name__ == "__main__":
     vae = ConvolutionalVAE(
+        (1, 28, 28),
         [1, 32, 64, 128, 128], 
         [None, 3, 2, 2, 3], 
         [None, 1, 2, 2, 1], 
         [None, 1, 0, 0, 1], 
         nn.LeakyReLU(),
         20,
-        (128, 7, 7),
         [128, 128, 64, 32, 1],
         [3, 2, 2, 3, None],
         [1, 2, 2, 1, None],
